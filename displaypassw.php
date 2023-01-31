5de52b82c444dc2158c2a22c52d5e34504ba51a0
Frah01 <vinario.francesco@gmail.com>
2023-01-31 19:41:55 +0100

    Aggiunto controllo per numeri negativi

--- displaypassw.php
+++ displaypassw.php
@@ -6,7 +6,7 @@ $_SESSION['password'] = $_GET['password'];
 
 
 
-if (empty($_SESSION['password'])) {
+if (empty($_SESSION['password']) || $_SESSION['password'] < 0) {
     header('Location:./index.php');
 }
 ?>
@@ -20,7 +20,7 @@ if (empty($_SESSION['password'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>Document</title>
+    <title>La tua password</title>
 </head>
 
 <body class="background-body">
--- functions.php
+++ functions.php
@@ -1,11 +1,15 @@
-<?php 
+<?php
 
 $passwGen = $_GET['password'];
-$randomText = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+$randomText = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?^*@%&£';
 
 function randomGen($passwGen, $randomText)
 {
     if (isset($_GET['password'])) {
-        return substr(str_shuffle($randomText), 0, $_GET['password']);
+        if ($_GET['password'] < 0) {
+            return 'Inserire un numero positivo';
+        } else {
+            return substr(str_shuffle($randomText), 0, $_GET['password']);
+        }
     }
 }
