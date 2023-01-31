0e09a5271abb64ea0b0beee97caafaacefee00bc
Frah01 <vinario.francesco@gmail.com>
2023-01-31 15:18:35 +0100

    Esercizio completo con Milestone 3 (bonus)

--- displaypassw.php
+++ displaypassw.php
@@ -1,7 +1,14 @@
 <?php
 
-include __DIR__ . '/functions.php'
+include __DIR__ . '/functions.php';
+session_start();
+$_SESSION['password'] = $_GET['password'];
 
+
+
+if (empty($_SESSION['password'])) {
+    header('Location:./index.php');
+}
 ?>
 
 <!DOCTYPE html>
@@ -11,21 +18,25 @@ include __DIR__ . '/functions.php'
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Document</title>
 </head>
 
-<body>
-    <div class="container">
+<body class="background-body">
+    <div class="container mt-5">
         <div class="row">
             <div class="col">
-                <div class="form-group">
+                <div class="text-center text-white background-topbar p-3">
+                    <h3>Strong Password Generator</h3>
+                </div>
+                <div class="text-center bg-white d-flex justify-content-center p-3">
                     <div>
-                        <h3>La tua password</h3>
-                        <?php
-
-                        echo randomGen($passwGen, $randomText);
-
-                        ?>
+                        <h3>La tua Password è :</h3>
+                    </div>
+                    <div class="mx-2">
+                        <h3><?php echo randomGen($passwGen, $randomText); ?>
+                        </h3>
                     </div>
                 </div>
             </div>
--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<?php
-
-include __DIR__ . '/functions.php'
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,22 +5,30 @@ include __DIR__ . '/functions.php'
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Strong password generator</title>
 </head>
 
-<body>
-    <div class="container mt-5">
-        <div class="row">
+<body class="background-body">
+    <div class="container  mt-5">
+        <div class="row background-topbar">
+            <div class="col">
+                <div class="text-center text-white p-3">
+                    <h1>Strong Password Generator</h1>
+                </div>
+            </div>
+        </div>
+        <div class="row bg-white p-5">
             <div class="col-6 d-flex justify-content-center">
-                <h3>Lunghezza password</h3>
+                <h3>Scegli la lunghezza della tua password</h3>
             </div>
             <div class="col-6 d-flex justify-content-center">
                 <form action="./displaypassw.php" method="GET">
-                    <div class="form-group">
+                    <div class="form-group d-flex">
                         <input class="form-control" type="number" name="password">
+                        <button type="submit" class="btn btn-dark mx-2">Invia</button>
                     </div>
-
                 </form>
             </div>
         </div>
