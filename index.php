0aaa004a993a902305049e4488e10dd995d66c8a
Frah01 <vinario.francesco@gmail.com>
2023-01-31 14:22:55 +0100

    Milestone 2 completata

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
-<?php 
-$passwGen = $_GET['password'];
-$randomText = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+<?php
 
-function randomGen( $passwGen , $randomText ) {
-    return substr( str_shuffle( $randomText ), 0,$_GET['password']);
-}
+include __DIR__ . '/functions.php'
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -17,6 +14,7 @@ function randomGen( $passwGen , $randomText ) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Strong password generator</title>
 </head>
+
 <body>
     <div class="container mt-5">
         <div class="row">
@@ -24,20 +22,24 @@ function randomGen( $passwGen , $randomText ) {
                 <h3>Lunghezza password</h3>
             </div>
             <div class="col-6 d-flex justify-content-center">
-                <form action="./index.php" method="GET" >
-                    <div class="form-group" >
+                <form action="./index.php" method="GET">
+                    <div class="form-group">
                         <input class="form-control" type="number" name="password">
                     </div>
-                    <div class="form-group" >
-                       <?php 
-                       
-                       echo randomGen($passwGen , $randomText);
+                    <div class="form-group">
+                        <div>
+                            <h3>La tua password</h3>
+                            <?php
 
-                       ?>
+                            echo randomGen($passwGen, $randomText);
+
+                            ?>
+                        </div>
                     </div>
                 </form>
             </div>
         </div>
     </div>
 </body>
+
 </html>
